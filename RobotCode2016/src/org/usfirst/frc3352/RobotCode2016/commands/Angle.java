d9e50a49428dead9795d9a50f3627bba461d5250
Flybat <zhoegberg@gmail.com>
2016-03-05 15:26:17 -0600

    Example

--- RobotCode2016/src/org/usfirst/frc3352/RobotCode2016/commands/Angle.java
+++ RobotCode2016/src/org/usfirst/frc3352/RobotCode2016/commands/Angle.java
@@ -38,6 +38,7 @@ public class Angle extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	
     }
 
     // Called repeatedly when this Command is scheduled to run
