2a1a5458364c7b48e45b75962b5f1a8420a973ab
gravitysandwich <jackrudawski@gmail.com>
2016-03-08 20:41:39 -0600

    Added UNTESTED BallShifter Code. I am not sure if this is how it's even suppose to operate.

---
+++ RobotCode2016/src/org/usfirst/frc3352/RobotCode2016/commands/ShiftGear.java
@@ -0,0 +1,50 @@
+package org.usfirst.frc3352.RobotCode2016.commands;
+
+import org.usfirst.frc3352.RobotCode2016.Robot;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ShiftGear extends Command {
+
+    public ShiftGear() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.gearshift);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() 
+    {
+    	if (Robot.gearshift.ShiftState() == Value.kForward)
+    	{
+    		Robot.gearshift.ShiftDown();
+    	}
+    	else
+    	{
+    		Robot.gearshift.ShiftUp();
+    	}
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
--- RobotCode2016/src/org/usfirst/frc3352/RobotCode2016/subsystems/GearShifter.java
+++ RobotCode2016/src/org/usfirst/frc3352/RobotCode2016/subsystems/GearShifter.java
@@ -1,36 +1,38 @@
 package org.usfirst.frc3352.RobotCode2016.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
-
 import org.usfirst.frc3352.RobotCode2016.RobotMap;
-
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 /**
  *
  */
-public class ShooterPneumatics extends Subsystem {
+public class GearShifter extends Subsystem {
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-	private final DoubleSolenoid BallPusher = RobotMap.pushball;
-
+	private final DoubleSolenoid Shifter = RobotMap.gearshift;
+	
+	
+	
+	public void ShiftUp()
+	{
+		Shifter.set(Value.kForward);
+	}
+	
+	public void ShiftDown()
+	{
+		Shifter.set(Value.kReverse);
+	}
+	
+	public Value ShiftState()
+	{
+		return Shifter.get();	
+	}
+	
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
-    	
     }
-    public void push()
-    {
-    	BallPusher.set(DoubleSolenoid.Value.kForward);
-    }
-    public void pull()
-    {
-    	BallPusher.set(DoubleSolenoid.Value.kReverse);
-    }
-    public void stop()
-    {
-    	BallPusher.set(DoubleSolenoid.Value.kOff);
-    }
-}
+}	
 
